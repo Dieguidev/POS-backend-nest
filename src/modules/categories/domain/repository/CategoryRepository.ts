b6054a39d4c04b8e6f49aa1d3f6e24faeef753a9
Dieguidev <diegogaraycullas@gmail.com>
2025-02-06 10:24:20 -0500

    Refactor repositorio de categorías: cambiar la interfaz a una clase abstracta para mejorar la extensibilidad

--- src/modules/categories/domain/repository/CategoryRepository.ts
+++ src/modules/categories/domain/repository/CategoryRepository.ts
@@ -1,11 +1,11 @@
 import { CreateCategoryDto } from "../../application/dto/create-category.dto";
 import { Category } from "../entities/category.entity";
 
-export interface CategoryRepository {
-  createCategory(dto: CreateCategoryDto): Promise<string>;
-  updateCategory(id: number, name: string): Promise<void>;
-  deleteCategory(id: number): Promise<void>;
-  findCategoryById(id: number): Promise<Category>;
-  findAllCategories(): Promise<Category[]>;
+export abstract class CategoryRepository {
+  abstract  createCategory(dto: CreateCategoryDto): Promise<string>;
+  abstract  updateCategory(id: number, name: string): Promise<void>;
+  abstract  deleteCategory(id: number): Promise<void>;
+  abstract  findCategoryById(id: number): Promise<Category>;
+  abstract  findAllCategories(): Promise<Category[]>;
 }
 
