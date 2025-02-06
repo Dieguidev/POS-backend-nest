a2b6083f2c1f37ff9ceb744e7d7eba25a2613430
Dieguidev <diegogaraycullas@gmail.com>
2025-02-06 10:23:22 -0500

    Agregar controladores de autenticación: implementar métodos de inicio de sesión y registro, y crear DTO para la validación de credenciales

--- src/modules/auth/infrastructure/controllers/auth.controller.ts
+++ src/modules/auth/infrastructure/controllers/auth.controller.ts
@@ -1,11 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from '../../application/service/auth.service';
 import { UpdateAuthDto } from '../../application/dto/update-auth.dto';
-
+import { CreateUserDto } from '../../application/dto/create-user.dto';
+import { LoginDto } from '../../application/dto/login-dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.authService.validateUser(
+      loginDto.email,
+      loginDto.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+    return this.authService.login(user);
+  }
 
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto.email, dto.password);
+  }
 }
