b09f5e2aaac0ebc6e03220a0a2c8cec3a3e93d20
Dieguidev <diegogaraycullas@gmail.com>
2025-02-06 11:57:14 -0500

    Refactor repositorio de usuarios para incluir roles en las consultas y agregar implementación de CategoriesRepositoryImpl para la gestión de categorías

--- src/modules/auth/infrastructure/repositoriesImpl/prisma-user.repository.ts
+++ src/modules/auth/infrastructure/repositoriesImpl/prisma-user.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { UserRepository } from 'src/modules/auth/domain/repositories/user.repository';
 import { User } from 'src/modules/auth/domain/entities/user.entity';
-import { CreateUserDto } from 'src/modules/auth/application/dto/create-user.dto';
+
 import { PrismaService } from 'src/shared/database/prisma.service';
 
 
@@ -11,17 +11,45 @@ export class PrismaUserRepository implements UserRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findByEmail(email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { email }, include: { roles: true } });
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      include: { roles: { include: { role: true } } },
+    });
+    return this.mapUser(user);
   }
 
   async create(email:string, password: string): Promise<User> {
-    return this.prisma.user.create({ data: {
+    const user =await this.prisma.user.create({ data: {
       password,
-      email
-    }, include: { roles: true } });
+      email,
+      roles: {
+        create: [
+          {
+            roleId:1
+          }
+        ]
+      }
+    }, include: { roles: { include: { role: true } } } });
+    console.log(user);
+
+    return this.mapUser(user);
   }
 
   async findById(id: number): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { id }, include: { roles: true } });
+    const user =await  this.prisma.user.findUnique({ where: { id }, include: { roles:{ include: { role: true } } } });
+    return this.mapUser(user);
+  }
+
+  private mapUser(user: any): User | null {
+    if (!user) return null;
+    return {
+      ...user,
+      roles: user.roles.map((userRole: any) => ({
+        id: userRole.role.id,
+        name: userRole.role.name,
+        createdAt: userRole.role.createdAt,
+        updatedAt: userRole.role.updatedAt,
+      })),
+    };
   }
 }
