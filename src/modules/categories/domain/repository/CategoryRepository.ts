bc8e919c074a21f85c2b1c82935ba0cf4d8ff2b7
Dieguidev <diegogaraycullas@gmail.com>
2025-02-05 23:25:02 -0500

    Actualizar el repositorio de categorías para usar DTO en la creación de categorías y manejar la creación a través de Prisma

--- src/modules/categories/domain/repository/CategoryRepository.ts
+++ src/modules/categories/domain/repository/CategoryRepository.ts
@@ -1,7 +1,8 @@
+import { CreateCategoryDto } from "../../application/dto/create-category.dto";
 import { Category } from "../entities/category.entity";
 
 export interface CategoryRepository {
-  createCategory(name: string): Promise<void>;
+  createCategory(dto: CreateCategoryDto): Promise<string>;
   updateCategory(id: number, name: string): Promise<void>;
   deleteCategory(id: number): Promise<void>;
   findCategoryById(id: number): Promise<Category>;
--- src/modules/categories/infrastructure/repository/db/categoriesRepositoryImpl.ts
+++ src/modules/categories/infrastructure/repository/db/categoriesRepositoryImpl.ts
@@ -1,12 +1,30 @@
 import { Injectable } from "@nestjs/common";
+import { CreateCategoryDto } from "src/modules/categories/application/dto/create-category.dto";
 import { Category } from "src/modules/categories/domain/entities/category.entity";
 import { CategoryRepository } from "src/modules/categories/domain/repository/CategoryRepository";
+import { PrismaService } from "src/shared/database/prisma.service";
 
 @Injectable()
 export class CategoriesRepositoryImpl implements CategoryRepository {
-  createCategory(name: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async createCategory(dto: CreateCategoryDto): Promise<string> {
+    const {name} = dto
+    const category =await this.prisma.category.create({
+      data: {
+        name
+      }
+    })
+
+    if(!category){
+      throw new Error('Error creating category')
+    }
+    return 'Category created successfully'
   }
+
   updateCategory(id: number, name: string): Promise<void> {
     throw new Error("Method not implemented.");
   }
