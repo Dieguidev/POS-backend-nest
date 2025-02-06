5ecc35dbb7369ee1012163e8913d6fdfaec2718a
Dieguidev <diegogaraycullas@gmail.com>
2025-02-06 10:26:10 -0500

    Eliminar método estático obsoleto de conversión de JSON en la entidad de categoría

--- src/modules/categories/domain/entities/category.entity.ts
+++ src/modules/categories/domain/entities/category.entity.ts
@@ -7,19 +7,5 @@ export class Category {
   createdAt: Date;
   updatedAt: Date;
 
-  // static fromJson(object: { [key: string]: any }): CategoryEntity {
-  //   const { id, name, createdAt, updatedAt } = object;
-
-  //   // if (!id) throw CustomError.badRequest('Missing ID');
-  //   // if (!name) throw CustomError.badRequest('Missing name');
-  //   // if(!email) throw CustomError.badRequest('Missing email');
-
-  //   // const budgetsFromJson = budgets
-  //   //   ? budgets.map((budget: Budget) => BudgetEntity.fromJson(budget))
-  //   //   : budgets;
-
-
-  //   return new CategoryEntity(id, name, email,  createdAt, updatedAt );
-  // }
 }
 
