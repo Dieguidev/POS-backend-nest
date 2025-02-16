87937d47eb663348a8160f7d26921cdf7a123a9a
Dieguidev <diegogaraycullas@gmail.com>
2025-02-16 08:15:40 -0500

    Agregar configuración de Cloudinary y modificar el módulo de carga de imágenes

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,18 +1,24 @@
+import 'dotenv/config';
 
-import 'dotenv/config'
-
-import * as joi from 'joi'
+import * as joi from 'joi';
 
 interface EnvVars {
-  PORT: number
-  JWT_SECRET: string
+  PORT: number;
+  JWT_SECRET: string;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
 }
 
-const envsSchema = joi.object({
-  PORT: joi.number().required(),
-  JWT_SECRET: joi.string().required()
-})
-.unknown(true);
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    JWT_SECRET: joi.string().required(),
+    CLOUDINARY_CLOUD_NAME: joi.string().required(),
+    CLOUDINARY_API_KEY: joi.string().required(),
+    CLOUDINARY_API_SECRET: joi.string().required(),
+  })
+  .unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
 
@@ -20,12 +26,12 @@ if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
-
 const envVars: EnvVars = value;
 
 export const envs = {
   jwtSecret: envVars.JWT_SECRET,
-  port: envVars.PORT
-}
-
-
+  port: envVars.PORT,
+  cloudinaryCloudName: envVars.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: envVars.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: envVars.CLOUDINARY_API_SECRET,
+};
--- src/modules/upload-image/upload-image.ts
+++ src/modules/upload-image/upload-image.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {v2 as cloudinary} from 'cloudinary'
+import { envs } from 'src/config/envs'
 
-@Injectable()
-export class UploadImage {}
+export const UploadImageProvider = {
+  provide: 'CLOUDINARY',
+  useFactory: () => {
+    return cloudinary.config({
+      cloud_name: envs.cloudinaryCloudName,
+      api_key: envs.cloudinaryApiKey,
+      api_secret: envs.cloudinaryApiSecret
+    })
+  }
+}
